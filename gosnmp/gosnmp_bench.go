f84dc2b6e8ff40fc205aef9fadd8996b1c0f8088
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-10-01 19:25:40 +0200

    update gosnmp/gosnmp_bench.go

--- gosnmp/gosnmp_bench.go
+++ gosnmp/gosnmp_bench.go
@@ -27,52 +27,67 @@ func main() {
 	if err != nil {
 		log.Fatalf("port arg err: %v", err)
 	}
-	_, err = strconv.Atoi(os.Args[4]) // first command line parameter, ...
-// 	sessions_num
+	sessions_num, err := strconv.Atoi(os.Args[4]) // first command line parameter, ...
 	if err != nil {
 		log.Fatalf("sessions arg err: %v", err)
 	}
 
-	// Default is a pointer to a GoSNMP struct that contains sensible defaults
-	// eg port 161, community public, etc
-	g.Default.Target = host
-	g.Default.Port = uint16(port)
-	g.Default.MaxOids = 100
-	err = g.Default.Connect()
-	if err != nil {
-		log.Fatalf("Connect() err: %v", err)
-	}
-	defer g.Default.Conn.Close()
+	var sessions []g.GoSNMP
 
-	start := time.Now()
+	for i := 0; i < sessions_num; i++ {
+		session := g.GoSNMP{
+			Target: host,
+			Port:               uint16(port),
+			Community:          "public",
+			Version:            g.Version2c,
+			Timeout:            time.Duration(2) * time.Second,
+			Retries:            3,
+			ExponentialTimeout: true,
+			MaxOids:            100,
+		}
+
+		err = session.Connect()
+		if err != nil {
+			log.Fatalf("Connect() err: %v", err)
+		}
+		defer session.Conn.Close()
+		sessions = append(sessions, session)
+	}
 
 	oids := []string{}
 	for i := 1; i <= oid_batch_size; i++ {
 		oids = append(oids, "1.3.6.1.2.1.25.6.3.1.1." + strconv.Itoa(i))
 	}
-	result, err2 := g.Default.Get(oids) // Get() accepts up to g.MAX_OIDS
 
+	var results []*g.SnmpPacket
+
+	start := time.Now()
+	for i := 0; i < len(sessions); i++ {
+		result, err2 := sessions[i].Get(oids)
+		if err2 != nil {
+			log.Fatalf("Get() err: %v", err2)
+		}
+		results = append(results, result)
+	}
 	elapsed := time.Since(start)
 
+	for i, result := range results {
+		fmt.Printf("Session: %d\n", i)
 
+		for j, variable := range result.Variables {
+			fmt.Printf("%d: oid: %s ", j, variable.Name)
 
-	if err2 != nil {
-		log.Fatalf("Get() err: %v", err2)
-	}
-
-	for i, variable := range result.Variables {
-		fmt.Printf("%d: oid: %s ", i, variable.Name)
-
-		// the Value of each variable returned by Get() implements
-		// interface{}. You could do a type switch...
-		switch variable.Type {
-		case g.OctetString:
-			fmt.Printf("string: %s\n", string(variable.Value.([]byte)))
-		default:
-			// ... or often you're just interested in numeric values.
-			// ToBigInt() will return the Value as a BigInt, for plugging
-			// into your calculations.
-			fmt.Printf("number: %d\n", g.ToBigInt(variable.Value))
+			// the Value of each variable returned by Get() implements
+			// interface{}. You could do a type switch...
+			switch variable.Type {
+			case g.OctetString:
+				fmt.Printf("string: %s\n", string(variable.Value.([]byte)))
+			default:
+				// ... or often you're just interested in numeric values.
+				// ToBigInt() will return the Value as a BigInt, for plugging
+				// into your calculations.
+				fmt.Printf("number: %d\n", g.ToBigInt(variable.Value))
+			}
 		}
 	}
 
